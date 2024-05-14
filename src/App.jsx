2deb6b3eacc49ef9c4af6d437c6ccf02ec9645b8
KwlsQ <vincentiussss@gmail.com>
2024-05-14 17:41:15 +0700

    card default styling and formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar'
+import Cards from './components/cards'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Cards/>
     </>
   )
 }
