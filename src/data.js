0a5416d80b5dc6b7d5b07dfdfd3d6669f212d2a9
KwlsQ <vincentiussss@gmail.com>
2024-05-14 13:04:04 +0700

    populate data

--- src/data.js
+++ src/data.js
@@ -1,5 +1,6 @@
 export default [
     {
+        id: 1,
         title: "Mount Fuji",
         location: "Japan",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
@@ -7,5 +8,25 @@ export default [
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
         imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
-    }
+    },
+    {
+        id: 2,
+        title: "Osaka tower",
+        location: "Japan",
+        googleMapsUrl: "https://maps.app.goo.gl/ASgSq7iRkoWnUpDe9",
+        startDate: "13 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Osaka Tower was an observation and radio tower built beside the headquarters of Asahi Broadcasting Corporation in Kita-ku, Osaka, Japan",
+        imageUrl: "https://images.unsplash.com/photo-1590559899731-a382839e5549?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 3,
+        title: "Shibuya",
+        location: "Japan",
+        googleMapsUrl: "https://maps.app.goo.gl/jHoMhHS45CnzFXtr5",
+        startDate: "12 Mar, 2021",
+        endDate: "24 Apr, 2021",
+        description: "Shibuya is a special ward in the Tokyo Metropolis, Japan. A major commercial and finance center, Shibuya houses two of the busiest railway stations in the world, Shibuya Station and Shinjuku Station",
+        imageUrl: "https://images.unsplash.com/photo-1583930263826-a1c6a639eaf2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
 ]
